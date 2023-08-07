cafe827837b9f1493b5ed3c0f5072829c3b403af
anartzdev <105705996+anartzdev@users.noreply.github.com>
2023-08-07 07:37:01 +0200

    feat: add project in differents components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,103 +1,46 @@
-import { component$, useStore, $ } from '@builder.io/qwik';
+import { component$, useStore } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import { GameStore } from '~/models/game';
+import Choices from '~/components/game/choices';
+import { Title } from '~/components/game/title';
+import { ScoreBoard } from '~/components/game/score-board';
 
 export default component$(() => {
-  const game = useStore({
+  const game = useStore<GameStore>({
     data: {
       result: 'Waiting for the first play...',
       pointsUser: 0,
       pointsComp: 0,
     },
+    choices: {
+      imageProperties: {
+        width: 100,
+        height: 100,
+      },
+      rock: {
+        img: '/img/rock.png',
+        alt: 'Rock Image',
+      },
+      paper: {
+        img: '/img/paper.png',
+        alt: 'Pape Image',
+      },
+      scissors: {
+        img: '/img/scissors.png',
+        alt: 'Scissors Image',
+      },
+    },
   });
 
-  const getComputerChoice = $(async () => {
-    const choices = ['r', 'p', 's']; // Roca, Pape, Tijeras
-    const randomChoice = Math.floor(Math.random() * 3);
-    return choices[randomChoice];
-  });
-
-  const gamePlay = $(
-    async (userChoice: string): Promise<void> => {
-      const playUserComp = userChoice + (await getComputerChoice());
-      console.log(`Attempt in progress: ${playUserComp}`);
-      let playStatus: {
-        message: string;
-        userAdd: number;
-        compAdd: number;
-      } = {
-        message: '',
-        userAdd: 0,
-        compAdd: 0,
-      };
-      switch (playUserComp) {
-        // Ganamos
-        case 'rs':
-        case 'sp':
-        case 'pr':
-          playStatus = {
-            message: 'You beat the computer',
-            userAdd: 1,
-            compAdd: 0,
-          };
-          break;
-        // Gana la computadora
-        case 'rp':
-        case 'ps':
-        case 'sr':
-          playStatus = {
-            message: 'Win the computer',
-            userAdd: 0,
-            compAdd: 1,
-          };
-          break;
-        // Empatamos
-        case 'rr':
-        case 'pp':
-        case 'ss':
-          playStatus = {
-            message: 'You have chosen the same play and you have tied',
-            userAdd: 0,
-            compAdd: 0,
-          };
-          break;
-      }
-
-      game.data = {
-        ...game.data,
-        result: playStatus.message,
-        pointsUser: game.data.pointsUser + playStatus.userAdd,
-        pointsComp: game.data.pointsComp + playStatus.compAdd,
-      };
-    }
-  );
   return (
     <>
-      <div class='title'>
-        <h1>Rock, Paper or Scissor!!</h1>
-      </div>
-      <div class='score-board'>
-        <div id='user-label' class='badge'>
-          user
-        </div>
-        <div id='comp-label' class='badge'>
-          comp
-        </div>
-        <span id='user-score'>{game.data.pointsUser}</span>:
-        <span id='comp-score'>{game.data.pointsComp}</span>
-      </div>
+      <Title />
+      <ScoreBoard user={game.data.pointsUser} computer={game.data.pointsComp} />
+
       <p class='info-game'>{game.data.result}</p>
 
-      <div class='choices'>
-        <div class='choice' onClick$={() => gamePlay('r')}>
-          <img width={100} height={100} src='/img/rock.png' alt='rock image' />
-        </div>
-        <div class='choice' onClick$={() => gamePlay('p')}>
-          <img width={100} height={100} src='/img/paper.png' alt='paper image' />
-        </div>
-        <div class='choice' onClick$={() => gamePlay('s')}>
-          <img width={100} height={100} src='/img/scissors.png' alt='Scissors image' />
-        </div>
-      </div>
+      <Choices game={game} />
+
       <p class='info-game'>Select your play</p>
     </>
   );
